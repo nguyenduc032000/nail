4b798f79c31f9eb838f49f1d2e6aece94f85997b
nguyenduc032000 <153576279+nguyenduc032000@users.noreply.github.com>
2023-12-23 21:49:59 +0700

    Delete Prj-main/Prj-main directory

--- Prj-main/Prj-main/app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var contents = document.querySelectorAll(".content");
-
-    contents.forEach(function (content) {
-        content.style.opacity = 1;
-        content.style.transform = "translateY(0)";
-    });
-});
